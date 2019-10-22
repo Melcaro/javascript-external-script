5132ecfb0a007303e0890cd93ed62956df167482
melanie caro <melanie.caro@stackerine.com>
2019-10-22 11:04:14 +0200

    creating commands on userQueries.js

---
+++ userQueries.js
@@ -0,0 +1,8 @@
+// variables to ask the user his/her first name, last name and age.
+const firstName = prompt('Quel est votre prénom ?');
+const lastName = prompt('Quel est votre nom ?');
+const age = prompt('Quel âge avez-vous ?');
+
+console.log(
+  'Bonjour ' + firstName + ' ' + lastName + '. Vous avez ' + age + 'ans.'
+);
